21457b7025f537370c74d6f821738e982662a4c5
PARTIELS <partiels@GAMINGCAMPUS.local>
2022-01-18 11:19:55 +0100

    Take out fire charge

---
+++ Assets/Scripts/SO_Cooldown.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cooldown", menuName = "ScriptableObjects/Cooldown")]
+public class SO_Cooldown : ScriptableObject
+{
+    public float fCooldown = 10;
+}
