5e2ac1bf9edb80d780ba58015b3e490b9bbf704a
Yoorwan <74179029+Yoorwan@users.noreply.github.com>
2022-02-03 12:23:49 +0100

    Delete BT dupe

--- Assets/Scripts/BehaviorTree/AbstractNodeScript/CompositeNode.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CompositeNode : BTNode
-{
-    public List<BTNode> children = new List<BTNode>();
-}
